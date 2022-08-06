095e7a79bc697f761975dd5df2ca01f7dcb08795
eeevans <ed.eevans@gmail>
2022-08-06 14:01:56 -0500

    Changed saw blade script to make the speed independent of the distance travelled. Also made prefabs out of various objects

--- Assets/Scripts/SawBlade.cs
+++ Assets/Scripts/SawBlade.cs
@@ -5,14 +5,16 @@ public class SawBlade : MonoBehaviour
     [SerializeField] Transform _start;
     [SerializeField] Transform _end;
     [SerializeField] Transform _sawBladeSprite;
-    [SerializeField] float _speedFactor = 0.5f;
+    [SerializeField] float _speedFactor = 2f;
 
     private float _positionPercent;
     private int _direction = 1;
 
     private void Update()
     {
-        _positionPercent += Time.deltaTime * _direction * _speedFactor;
+        var distance = Vector3.Distance(_start.position, _end.position);
+        var speedOverDistance = _speedFactor / distance;
+        _positionPercent += Time.deltaTime * _direction * speedOverDistance;
         _sawBladeSprite.position = Vector3.Lerp(_start.position, _end.position, _positionPercent);
         if (_positionPercent >= 1 && _direction == 1)
             _direction = -1;
