205e31d09e2173ac5cc2decae49da5732bf9ea7a
eeevans <ed.eevans@gmail>
2022-08-07 00:39:50 -0500

    Added sticking to moving objects

--- Assets/Scripts/CharacterGrounding.cs
+++ Assets/Scripts/CharacterGrounding.cs
@@ -9,16 +9,41 @@ public class CharacterGrounding : MonoBehaviour, IGround
 
     public bool IsGrounded { get; private set; }
 
+    private Transform groundedObject;
+    private Vector3? groundedObjectLastPosition;
+
     private void Update()
     {
         CheckFootForGrounding(leftFoot);
         if (!IsGrounded)
             CheckFootForGrounding(rightFoot);
+
+        StickToMovingObjects();
+    }
+
+    private void StickToMovingObjects()
+    {
+        if (!(groundedObject is null))
+        {
+            if (groundedObjectLastPosition.HasValue &&
+                groundedObjectLastPosition.Value != groundedObject.position)
+            {
+                Vector3 delta = groundedObject.position - groundedObjectLastPosition.Value;
+                transform.position += delta;
+            }
+
+            groundedObjectLastPosition = groundedObject.position;
+        }
+        else
+        {
+            groundedObjectLastPosition = null;
+        }
     }
 
     private void CheckFootForGrounding(Transform foot)
     {
         var raycastHit = Physics2D.Raycast(foot.position, Vector2.down, maxDistance, layerMask);
         IsGrounded = !(raycastHit.collider is null);
+        groundedObject = raycastHit.collider?.transform;
     }
 }
