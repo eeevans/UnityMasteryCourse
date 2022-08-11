284d9cf15f6c778e008eb07f3239e94177032e71
eeevans <ed.eevans@gmail>
2022-08-11 14:32:21 -0500

    Added code to change walk direction when detecting an edge

--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class Walker : MonoBehaviour
 {
+    private const float Small_Distance = 0.1f;
+    private const float Negative_Direction = -1f;
     [SerializeField] float speed = 0.25f;
     Collider2D _collider;
     Rigidbody2D _rigidBody;
-    Vector2 direction;
+    Vector2 _direction = Vector2.left;
 
     void Awake()
     {
@@ -16,7 +18,31 @@ public class Walker : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        direction = Vector2.right;
-        _rigidBody.MovePosition(_rigidBody.position + direction * speed * Time.fixedDeltaTime);
+        _rigidBody.MovePosition(_rigidBody.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        if (ReachedEdge())
+            SwitchDirection();
+    }
+
+    private bool ReachedEdge()
+    {
+        float x = _direction.x == Negative_Direction ?  
+            _collider.bounds.min.x - Small_Distance : 
+            _collider.bounds.max.x + Small_Distance;
+        float y = _collider.bounds.min.y;
+
+        Vector2 origin = new Vector2(x, y);
+
+        var hit = Physics2D.Raycast(origin, Vector2.down, Small_Distance);
+        
+        return hit.collider == null;
+    }
+
+    private void SwitchDirection()
+    {
+        _direction *= Negative_Direction;
     }
 }
